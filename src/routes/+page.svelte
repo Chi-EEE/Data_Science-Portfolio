7f6558acb4e7d031516d7734d86376ecb6fc07cc
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-10-20 11:18:15 +0100

    Basic Info with deploy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Chi Huu Huynh</h1>
+<h2>Software Developer</h2>
+<div id="#education">
+    <h3>Education:</h3>
+    <h4>South East Technological University (IT Carlow) -</h4>
+    <a
+        href="https://www.itcarlow.ie/courses/type/undergraduate-cao-courses/computing-networking-courses/cw238.htm"
+    >
+        Bachelor of Software Development (Honours)
+    </a>
+    <br />
+    <small>November 2020- November 2024</small>
+    <p>
+        I am currently studying at SETU Carlow Campus (Institute Of Technology Carlow).
+        <br />
+        GPA of 1.1 (78.33%) in the 2nd year
+        <br />
+        GPA of 1.1 (82.4%) in the 3rd year
+    </p>
+</div>
+<div id="#work">
+    <h3>Work Experience:</h3>
+    <h4>Workday Internship</h4>
+    <small>April 2023- August 2023</small>
+    <p>
+        I have worked alongside other software engineers to work on the Toggle Automation project where I had to use various technologies to develop the Toggle Automation project.
+    </p>
+</div>
