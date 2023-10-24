e667a1fbe5fd78bee3f00ebd084d8a72350e1047
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-10-24 12:23:01 +0100

    Made Project into client only

---
+++ src/PreviewSection.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	interface Image {
+		src: string;
+		alt: string;
+	}
+	export let images: Image[];
+	let show = false;
+</script>
+
+<button
+	on:click={() => {
+		show = !show;
+	}}>{show ? "Show:" : "Hide:"}</button
+>
+{#if show}
+	{#each images as image}
+		<img src={image.src} alt={image.alt} />
+	{/each}
+{/if}
--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,5 @@
-import adapter from '@sveltejs/adapter-static'; // From: https://kit.svelte.dev/docs/adapter-static
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import sveltePreprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
-	}
+export default {
+  preprocess: sveltePreprocess()
 };
-
-export default config;
