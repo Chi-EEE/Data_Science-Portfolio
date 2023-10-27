82ef3c53f94c5226de7d80d1b36918e219d05944
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-10-27 17:42:31 +0100

    Generate CSS using ChatGPT

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,105 @@
 
 <svelte:head>
   <style>
+	/* START: Made by ChatGPT */
+	body {
+	font-family: Arial, sans-serif;
+	margin: 0;
+	padding: 0;
+	background-color: #f0f0f0;
+	}
+
+	/* Style the header */
+	header {
+	background-color: #333;
+	color: #fff;
+	text-align: center;
+	padding: 20px;
+	}
+
+	/* Style the navigation menu */
+	nav ul {
+	list-style: none;
+	padding: 0;
+	text-align: center;
+	}
+
+	nav li {
+	display: inline;
+	margin-right: 20px;
+	}
+
+	nav a {
+	text-decoration: none;
+	color: #fff;
+	}
+
+	/* Style the main content section */
+	main {
+	padding: 20px;
+	background-color: #fff;
+	box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+	}
+
+	/* Style the portfolio items */
+	section {
+	margin: 20px;
+	padding: 20px;
+	background-color: #fff;
+	box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+	}
+
+	/* Style the footer */
+	footer {
+	background-color: #333;
+	color: #fff;
+	text-align: center;
+	padding: 10px;
+	}
+
+	/* Apply styles to links within the content */
+	a {
+	color: #0078d4;
+	text-decoration: none;
+	}
+
+	/* Style headings */
+	h1, h2, h3 {
+	font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+	color: #333;
+	}
+
+	/* Apply styles to images */
+	img {
+	max-width: 100%;
+	height: auto;
+	}
+
+	/* Style unordered lists */
+	ul {
+	list-style: disc;
+	margin-left: 20px;
+	}
+
+	/* Apply styles to paragraphs */
+	p {
+	line-height: 1.6;
+	}
+
+	/* Apply styles to buttons */
+	button {
+	background-color: #0078d4;
+	color: #fff;
+	padding: 10px 20px;
+	border: none;
+	cursor: pointer;
+	transition: background-color 0.3s;
+	}
+
+	button:hover {
+	background-color: #005a9e;
+	}	
+	/* END */
     body {
       font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
         sans-serif;
@@ -19,7 +118,7 @@
 <div style="display: flex; flex-direction: row;">
   <section style="width: 50%;" id="main">
     <h1>Chi Huu Huynh</h1>
-    <h2>Software Developer</h2>
+    <sub>Software Developer</sub>
     <h3 id="table_of_contents">Table of Contents:</h3>
     <ul>
       {#each categories as category}
--- static/markdown/clash_royale_deck_suggester.md
+++ static/markdown/clash_royale_deck_suggester.md
@@ -8,7 +8,7 @@ Firstly, I removed the columns that I didn't need: Player tag, their ranking and
 This reduced the amount of columns from 75 to 36 headings and the size of the file got reduced to 35% of its original size.
 
 Here is a bar chart showing the number of cards used in the dataset:  
-<img src="graphs/bar_chart_1.png" width="1000px"><br>
+<img src="graphs/bar_chart_1.png"><br>
 
 Since these are ladder matches, cards can be underleveled / overleveled and can lead to bias in the win percentage of the matches. Since some cards can be more powerful when overleveled against underleveled cards.
 
